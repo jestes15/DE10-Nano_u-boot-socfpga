d4cb24ff1665e2975951fea9f3889b90050b003f
Joshua Estes <jne0011@auburn.edu>
2025-02-13 22:12:32 -0500

    Added mac address

--- include/configs/socfpga_common.h
+++ include/configs/socfpga_common.h
@@ -174,6 +174,7 @@
 	"pxefile_addr_r=0x02200000\0" \
 	"ramdisk_addr_r=0x02300000\0" \
 	"socfpga_legacy_reset_compat=1\0" \
+	"ethaddr=e6:e2:1b:3c:22:e8\0" \
 	"prog_core=if load mmc 0:1 ${loadaddr} fit_spl_fpga.itb;" \
 		   "then fpga loadmk 0 ${loadaddr}:fpga-core-1; fi\0" \
 	SOCFPGA_BOOT_SETTINGS \
